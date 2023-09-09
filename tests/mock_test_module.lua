3381f21f881c51e86cca7dde693003545a8bbd7f
Alex Veden <i@alexveden.com>
2023-09-09 14:14:48 +0400

    doc

--- tests/mock_test_module.lua
+++ tests/mock_test_module.lua
@@ -2,6 +2,8 @@ M = {}
 
 
 function M.param(s)
+    -- This module uses some global function
+    --   intended just for testing if Mock works on imported modules as well
     return getInfoParam(s)
 end
 
