63afc9e695425230a38632792e248e71d362249a
Alex Veden <i@alexveden.com>
2023-09-09 13:51:47 +0400

    implemented Mock.object

--- tests/Mock.lua
+++ tests/Mock.lua
@@ -1,10 +1,13 @@
 --
--- Simple LUA mocks
+-- Simple LUA mocks with pythonic flavor
 --
+--   Aleksandr Vedeneev (c) 2023 
+--
+--   MIT License
 --
 
 ---@class Mock
----@field name? string name of the mocked function or object
+---@field func_path? string name of the mocked function or object
 ---@field return_value any return value of the mock function
 ---@field side_effect nil| function | table custom function when mock called
 ---@field call_count number number of times mock was called
@@ -28,24 +31,43 @@ local GLOBAL_MOCKS_FORBIDDEN = {
 	["pairs"] = true,
 	["assert"] = true,
 }
+local MOCKED_OBJECTS = {}
 
+---Counts lenght of a table
+---@param tbl table
+---@return number
+local function tlen(tbl)
+	assert(type(tbl) == "table")
+	local cnt = 0
+	for _, _ in pairs(tbl) do
+		cnt = cnt + 1
+	end
+	return cnt
+end
+
+---Patches table function
+---@param tbl table table to patch (can be also _G)
+---@param fpath string func name or path ("os.clock", "class.sub.func")
+---@param create_missing boolean creates if not exist
+---@param table_name any tbl meaningful name
+---@param patch_func function patch function 
+---@return function|nil
 local function patch_table(tbl, fpath, create_missing, table_name, patch_func)
+	if not fpath or #fpath == 0 then
+		error("Empty func path ``, in table " .. table_name)
+	end
+
 	local _is_missing = false
 	local _tbl = tbl
 	local _t_orig_func = nil
 
-	-- name with dots!
 	for t in string.gmatch(fpath, "([^.]+)") do
 		if _tbl[t] == nil then
 			_is_missing = true
 
 			if not create_missing then
 				error(
-					string.format(
-						"%s[%s]function is not found (or try with create_missing=true).",
-						table_name,
-						fpath
-					)
+					string.format("%s[%s]function is not found (or try with create_missing=true).", table_name, fpath)
 				)
 			end
 		end
@@ -63,23 +85,56 @@ local function patch_table(tbl, fpath, create_missing, table_name, patch_func)
 		end
 	end
 
-	assert(_t_orig_func or create_missing, 'Expected to be patched or created, but not...')
+	assert(_t_orig_func or create_missing, "Expected to be patched or created, but not...")
 	return _t_orig_func
+end
+
+---Revert object patch
+---@param tbl table table to undo patch (can also be _G)
+---@param fpath string func name or path ("os.clock", "class.sub.func")
+---@param table_name string tbl meaningful name
+---@param orig_func function original object func (must be cached somewhere)
+---@return boolean
+local function unpatch_table(tbl, fpath, table_name, orig_func)
+	local _tbl = tbl
+
+	for t in string.gmatch(fpath, "([^.]+)") do
+		if _tbl[t] == nil then
+			error(string.format("%s[%s]function is not found.", table_name, fpath))
+		end
+
+		if type(_tbl[t]) == "table" then
+			_tbl = _tbl[t]
+		else
+			assert(type(_tbl[t]) == "function")
+
+			_tbl[t] = orig_func
+		end
+	end
+
+	return orig_func ~= nil
+end
+
+---Releases mocks and unpatch objects
+---@param mock_table table global mock cache
+---@param orig_object table object to unpatch
+---@param func_path string path to patched function
+local function release_mock(mock_table, orig_object, func_path)
+	if mock_table and mock_table[func_path] then
+		unpatch_table(orig_object, func_path, "release", mock_table[func_path])
+		mock_table[func_path] = nil
+	end
 end
 
 ---Creates mock for a global function
 ---@param global_name string global function name, for example 'print', 'os.clock', 'math.abs'
 ---@param create_missing? boolean force mock creation even if it's not found in _G (default: false)
 ---@return Mock
-function Mock.g(global_name, create_missing)
+function Mock.global(global_name, create_missing)
 	---@class Mock
 	local self = setmetatable({}, Mock)
-	self.name = global_name
+	self.func_path = global_name
 	self.call_args = {}
-
-	if not global_name or #global_name == 0 then
-		error("Empty global_name")
-	end
 
 	if GLOBAL_MOCKS_FORBIDDEN[global_name] then
 		error("Forbidden mocking for global `" .. global_name .. "`")
@@ -99,6 +154,54 @@ function Mock.g(global_name, create_missing)
 	end
 
 	return self
+end
+
+---Creates mock for a table's function
+---@param object table object to patch
+---@param func_path string global function name, for example 'print', 'os.clock', 'math.abs'
+---@param create_missing? boolean force mock creation even if it's not found in _G (default: false)
+---@return Mock
+function Mock.object(object, func_path, create_missing)
+	assert(type(object) == "table")
+
+	if MOCKED_OBJECTS[object] and MOCKED_OBJECTS[object][func_path] then
+		error(string.format("object(%s)[%s] already mocked my another mock.", tostring(object), func_path))
+	end
+
+	---@class Mock
+	local self = setmetatable({}, Mock)
+	self.func_path = func_path
+	self._object = object
+	self.call_args = {}
+
+	local mock_func = function(...)
+		return self.__call(self, ...)
+	end
+
+	local orig_func = patch_table(object, func_path, create_missing, "object", mock_func)
+
+	if not MOCKED_OBJECTS[object] then
+		MOCKED_OBJECTS[object] = {}
+	end
+
+	MOCKED_OBJECTS[object][func_path] = orig_func
+
+	return self
+end
+
+---Releases mock (unpatch global or object mock)
+function Mock:release()
+	if self._object then
+		-- looks line object mock
+		release_mock(MOCKED_OBJECTS[self._object], self._object, self.func_path)
+
+		if tlen(MOCKED_OBJECTS[self._object]) == 0 then
+			MOCKED_OBJECTS[self._object] = nil
+		end
+	else
+		-- Global cleanup
+		release_mock(GLOBAL_MOCKS, _G, self.func_path)
+	end
 end
 
 ---Reset mock call statistics
@@ -127,34 +230,29 @@ function Mock:__call(...)
 end
 
 ---Releases all aquired mocks
-function Mock.global_finalize()
-	for k, v in pairs(GLOBAL_MOCKS) do
-		if string.find(k, "%.") then
-			-- name with dots!
-			local _g = _G
-			for t in string.gmatch(k, "([^.]+)") do
-				if type(_g[t]) == "table" then
-					_g = _g[t]
-				else
-					_g[t] = v
-				end
-			end
-		else
-			_G[k] = v
-		end
+function Mock.release_all()
+	for k, _ in pairs(GLOBAL_MOCKS) do
+		release_mock(GLOBAL_MOCKS, _G, k)
+	end
 
-		GLOBAL_MOCKS[k] = nil
+	for obj, mock_table in pairs(MOCKED_OBJECTS) do
+		for func_path, _ in pairs(mock_table) do
+			release_mock(mock_table, obj, func_path)
+		end
+		MOCKED_OBJECTS[obj] = nil
 	end
 end
 
 ---Total count of currently mocked global functions
 ---@return number
 function Mock.global_count()
-	local cnt = 0
-	for _, _ in pairs(GLOBAL_MOCKS) do
-		cnt = cnt + 1
-	end
-	return cnt
+	return tlen(GLOBAL_MOCKS)
+end
+
+---Total count of currently mocked objects
+---@return number
+function Mock.objects_count()
+	return tlen(MOCKED_OBJECTS)
 end
 
 return Mock
--- tests/test_mock.lua
+++ tests/test_mock.lua
@@ -19,51 +19,124 @@ TestMock = {}
 function TestMock:setUp() end
 
 function TestMock:tearDown()
-	Mock.global_finalize()
+	Mock.release_all()
 	lu.assertEquals(Mock.global_count(), 0)
+	lu.assertEquals(Mock.objects_count(), 0)
 end
 
 function TestMock:test_mock_global__init()
-	local m = Mock.g("getInfoParam")
+	local m = Mock.global("getInfoParam")
 	lu.assertEquals(m.return_value, nil)
 	lu.assertEquals(m.call_count, 0)
-	lu.assertEquals(m.name, "getInfoParam")
+	lu.assertEquals(m.func_path, "getInfoParam")
 
-	lu.assertErrorMsgContains("function is not found (or try with create_missing=true).", Mock.g, "some-not_existing_global_func")
-	lu.assertErrorMsgContains("Global object was already mocked:", Mock.g, "getInfoParam")
-	lu.assertErrorMsgContains("Empty global_name", Mock.g, "")
-	lu.assertErrorMsgContains("Empty global_name", Mock.g, nil)
+	lu.assertErrorMsgContains(
+		"function is not found (or try with create_missing=true).",
+		Mock.global,
+		"some-not_existing_global_func"
+	)
+	lu.assertErrorMsgContains("Global object was already mocked:", Mock.global, "getInfoParam")
+	lu.assertErrorMsgContains("Empty func path ``, in table _G", Mock.global, "")
+	lu.assertErrorMsgContains("Empty func path ``, in table _G", Mock.global, nil)
 	lu.assertEquals(Mock.global_count(), 1)
 end
 
-function TestMock:test_mock_global_with_dots()
+function TestMock:test_mock_object__init()
+	local obj = {
+		test = 1,
+		add = function(a, b)
+			return a + b
+		end,
+	}
+
+	lu.assertEquals(obj.add(2, 3), 5)
+
+	local m = Mock.object(obj, "add")
+	lu.assertEquals(m.return_value, nil)
+	lu.assertEquals(m.call_count, 0)
+	lu.assertEquals(m.func_path, "add")
+
+	m.return_value = 77
+
+	lu.assertEquals(obj.add(1, 2), 77)
+	lu.assertEquals(m.call_count, 1)
+	lu.assertEquals(#m.call_args, 1)
+	lu.assertEquals(m.call_args[1], { 1, 2 })
+
+	m:release()
+	lu.assertEquals(obj.add(2, 3), 5)
+end
+
+function TestMock:test_mock_object__with_dots()
+	local obj = {
+		test = 1,
+		math = {
+			add = function(a, b)
+				return a + b
+			end,
+		},
+	}
+
+	lu.assertEquals(obj.math.add(2, 3), 5)
+
+	local m = Mock.object(obj, "math.add")
+	lu.assertEquals(m.return_value, nil)
+	lu.assertEquals(m.call_count, 0)
+	lu.assertEquals(m.func_path, "math.add")
+
+	m.return_value = 77
+
+	lu.assertEquals(obj.math.add(1, 2), 77)
+	lu.assertEquals(m.call_count, 1)
+	lu.assertEquals(#m.call_args, 1)
+	lu.assertEquals(m.call_args[1], { 1, 2 })
+
+	Mock.release_all()
+	lu.assertEquals(obj.math.add(2, 3), 5)
+end
+
+function TestMock:test_mock_global_with_dots__release_all()
 	-- WARNING: setting internal functions is possible but may lead to weird side effects!
-	local m = Mock.g("os.clock")
+	local m = Mock.global("os.clock")
 	m.return_value = 77.01
 	lu.assertEquals(m.return_value, 77.01)
 	lu.assertEquals(m.call_count, 0)
-	lu.assertEquals(m.name, "os.clock")
+	lu.assertEquals(m.func_path, "os.clock")
 
 	lu.assertEquals(os.clock(), 77.01)
 	lu.assertEquals(m.call_count, 1)
 
-	Mock.global_finalize()
+	Mock.release_all()
+	lu.assertNotEquals(os.clock(), 77.01)
+end
+
+function TestMock:test_mock_global_release_self()
+	local m = Mock.global("os.clock")
+	m.return_value = 77.01
+	lu.assertEquals(m.return_value, 77.01)
+	lu.assertEquals(m.call_count, 0)
+	lu.assertEquals(m.func_path, "os.clock")
+
+	lu.assertEquals(os.clock(), 77.01)
+	lu.assertEquals(m.call_count, 1)
+
+	m:release()
 	lu.assertNotEquals(os.clock(), 77.01)
 end
 
 function TestMock:test_mock_global__forbidden()
-	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.g, "error")
-	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.g, "setmetatable")
-	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.g, "type")
-	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.g, "pairs")
-	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.g, "assert")
+	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.global, "error")
+	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.global, "setmetatable")
+	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.global, "type")
+	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.global, "pairs")
+	lu.assertErrorMsgContains("Forbidden mocking for global ", Mock.global, "assert")
 end
 
 function TestMock:test_mock_global__init_create_non_existing()
-	local m = Mock.g("getInfoParamNoExists", true)
+	local m = Mock.global("getInfoParamNoExists", true)
 	lu.assertEquals(m.return_value, nil)
 	lu.assertEquals(m.call_count, 0)
-	lu.assertEquals(m.name, "getInfoParamNoExists")
+	lu.assertEquals(m.func_path, "getInfoParamNoExists")
 
 	--- This one now created by Mock!
 	---@diagnostic disable-next-line
@@ -72,10 +145,10 @@ function TestMock:test_mock_global__init_create_non_existing()
 end
 
 function TestMock:test_mock_global__reset_mock()
-	local m = Mock.g("getInfoParamNoExists", true)
+	local m = Mock.global("getInfoParamNoExists", true)
 	lu.assertEquals(m.return_value, nil)
 	lu.assertEquals(m.call_count, 0)
-	lu.assertEquals(m.name, "getInfoParamNoExists")
+	lu.assertEquals(m.func_path, "getInfoParamNoExists")
 
 	--- This one now created by Mock!
 	---@diagnostic disable-next-line
@@ -87,7 +160,7 @@ function TestMock:test_mock_global__reset_mock()
 end
 
 function TestMock:test_mock_call_from_another_module()
-	local m = Mock.g("getInfoParam", true)
+	local m = Mock.global("getInfoParam", true)
 	m.return_value = 124
 	local module_param = mock_test_module.param("mock test module")
 	lu.assertEquals(module_param, 124)
@@ -95,7 +168,7 @@ function TestMock:test_mock_call_from_another_module()
 end
 
 function TestMock:test_mock_side_effect_function()
-	local m = Mock.g("getInfoParam", true)
+	local m = Mock.global("getInfoParam", true)
 
 	-- IMPORTANT: return_value overriden by side_effect
 	m.return_value = 124
@@ -109,7 +182,7 @@ function TestMock:test_mock_side_effect_function()
 end
 
 function TestMock:test_mock_call_args()
-	local m = Mock.g("getInfoParam", true)
+	local m = Mock.global("getInfoParam", true)
 	m.return_value = 124
 	mock_test_module.param("param1")
 	mock_test_module.param("param2")
