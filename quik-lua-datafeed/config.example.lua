6db5da93252a12f9c27181bc4f74921a3b3b8daa
Alex Veden <i@alexveden.com>
2023-09-16 14:40:15 +0400

    config.example

--- quik-lua-datafeed/config.example.lua
+++ quik-lua-datafeed/config.example.lua
@@ -2,9 +2,10 @@
 local LoggerSocket = require('loggers.LoggerSocket')
 local LoggerPrintDbgStr = require('loggers.LoggerPrintDbgStr')
 
--- Варианты транспортов
+-- Варианты транспорта
 local TransportLog = require('transports.TransportLog')
 local TransportMemcached = require('transports.TransportMemcached')
+local TransportSocket = require('transports.TransportSocket')
 
 -- Handlers - это модули обработки информации от Квика
 local QuikStats = require('handlers.QuikStats')
@@ -12,8 +13,10 @@ local QuikStats = require('handlers.QuikStats')
 
 -- local logger = LoggerSocket.new({host = 'localhost', port = 17000})
 local logger = LoggerPrintDbgStr.new({})
+
 local transport = TransportLog.new({logger = logger})
 -- local transport = TransportMemcached.new({ host = "localhost", port = 11211, exptime_sec = 60*30})
+-- local transport = TransportSocket.new({host = 'localhost', port = 17001})
 
 CONFIG = {
     verbosity_level = 5,
